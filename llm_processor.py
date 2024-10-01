d25261af6e9084976f31b59433590607537d6175
VeeraSaiJoshik <JoshikDevelop@gmail.com>
2024-10-01 16:12:57 -0500

    Add Hallucination prevention flags

--- llm_processor.py
+++ llm_processor.py
@@ -4,11 +4,21 @@ import time
 
 class LLM_Processing :
   @staticmethod
+  def validate_response(response):
+    keywords = [('dont', 'context'), ('no', 'context'),
+                ('lack', 'context'), ('not', 'context'),
+                ('provided text', 'does not contain', 'any information regarding')]
+    contains_no_context = any(
+        all(word in response for word in keyword) for keyword in keywords)
+
+    if contains_no_context:
+      return "Sorry, but I do not have this information at the time. Please email or visit your counselor to get the accurate answer to your question!"
+
+    return response
+  
+  @staticmethod
   def post_process_output(raw_response: str) -> str :
-    formatted_response: str = raw_response.replace("```", "").replace(
-        "json", "").replace("{\n", "").replace('"Response" : "', "").replace('"Response": "', "").replace('"Response" :"', "").replace('"\n}', "")
-    
-    return formatted_response
+    return LLM_Processing.validate_response(json.loads(raw_response)["Response"])
   
   @staticmethod
   def get_natural_language_response(question: str, context: list[str], model: str):
@@ -30,7 +40,12 @@ class LLM_Processing :
             }
         ],
         model=model,
+        temperature=0.3,
+        max_tokens=356,
+        response_format={
+          "type": "json_object"
+        }
     )
-    response = chat_completion.choices[0].message.content
     
+    response = chat_completion.choices[0].message.content
     return LLM_Processing.post_process_output(response)
